b32b93f380071619eff63ee2c9864064b9b828f4
zarnigor87 <153763391+zarnigor87@users.noreply.github.com>
2024-03-18 20:36:59 -0500

    added rds

--- rds/main.tf
+++ rds/main.tf
@@ -2,3 +2,19 @@ resource "aws_db_subnet_group" "default" {
   name       = "main"
   subnet_ids = "data.terraform_remote_state.vpc.outputs.private_subnets"
 }
+
+
+resource "aws_db_instance" "default" {
+	identifier = "dbname"
+	allocated_storage = 20
+	storage_type = "gp2"
+	engine = "mysql"
+	engine_version = "5.7"
+	instance_class = "db.t2.micro"
+	name = "mydb"
+	username = "foo"
+	password = "foobarbaz"
+	publicly_accessible = true
+	db_subnet_group_name = aws_db_subnet_group.default.name
+	skip_final_snapshot = true #used to delete the repo in the future without this you cant delete. There are bugs reported 
+}
